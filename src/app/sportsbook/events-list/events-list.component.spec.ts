a75ccce16ad5633a44f770328180da1230523757
j4ckwinter <jackwinter.10@gmail.com>
2023-05-30 14:46:01 +0100

    Tests for component and events service

--- src/app/sportsbook/events-list/events-list.component.spec.ts
+++ src/app/sportsbook/events-list/events-list.component.spec.ts
@@ -1,24 +1,87 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EventsListComponent } from './events-list.component';
+import { EventsService } from '../service/events.service';
+import { BaseEvent, Event, MarketEvent } from '../model/event.model';
+import { of } from 'rxjs';
+import { MockModule } from 'ng-mocks';
+import { MatTableModule } from '@angular/material/table';
 
 describe('EventsListComponent', () => {
   let component: EventsListComponent;
   let fixture: ComponentFixture<EventsListComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [EventsListComponent],
-    }).compileComponents();
-  });
+  let eventsService: jasmine.SpyObj<EventsService>;
 
   beforeEach(() => {
+    const eventsServiceSpy = jasmine.createSpyObj('EventsService', [
+      'findAllEventIds',
+      'findEventDetails',
+      'findMarketDetails',
+    ]);
+
+    TestBed.configureTestingModule({
+      declarations: [EventsListComponent],
+      imports: [MockModule(MatTableModule)],
+      providers: [{ provide: EventsService, useValue: eventsServiceSpy }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(EventsListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    eventsService = TestBed.inject(
+      EventsService
+    ) as jasmine.SpyObj<EventsService>;
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update events when initialized', () => {
+    // Given
+    const baseEvent: BaseEvent = { id: 1 };
+    const event: Event = { id: 1, marketId: 2 } as Event;
+    const marketEvent: MarketEvent = {
+      id: 2,
+      home: 'Home',
+      away: 'Away',
+      draw: 'Draw',
+    } as MarketEvent;
+
+    eventsService.findAllEventIds.and.returnValue(of([baseEvent]));
+    eventsService.findEventDetails.and.returnValue(of(event));
+    eventsService.findMarketDetails.and.returnValue(of(marketEvent));
+
+    // When
+    component.ngOnInit();
+
+    // Then
+    expect(eventsService.findAllEventIds).toHaveBeenCalled();
+    expect(eventsService.findEventDetails).toHaveBeenCalledWith(baseEvent.id);
+    expect(eventsService.findMarketDetails).toHaveBeenCalledWith(
+      event.marketId
+    );
+    expect(component.events).toEqual([
+      {
+        ...event,
+        home: marketEvent.home,
+        away: marketEvent.away,
+        draw: marketEvent.draw,
+      },
+    ]);
+  });
+
+  it('should unsubscribe when component is destroyed', () => {
+    // Given
+    eventsService.findAllEventIds.and.returnValue(of([]));
+    eventsService.findEventDetails.and.returnValue(of());
+    eventsService.findMarketDetails.and.returnValue(of());
+
+    // When
+    component.ngOnInit();
+    component.ngOnDestroy();
+
+    // Then
+    expect(eventsService.findAllEventIds).toHaveBeenCalled();
+    expect(eventsService.findEventDetails).not.toHaveBeenCalled();
+    expect(eventsService.findMarketDetails).not.toHaveBeenCalled();
   });
 });
--- src/app/sportsbook/service/events.service.spec.ts
+++ src/app/sportsbook/service/events.service.spec.ts
@@ -1,16 +1,80 @@
 import { TestBed } from '@angular/core/testing';
-
 import { EventsService } from './events.service';
+import { WebsocketService } from './websocket.service';
+import { Observable } from 'rxjs';
+import { BaseEvent, Event, MarketEvent } from '../model/event.model';
 
 describe('EventsService', () => {
-  let service: EventsService;
+  let eventsService: EventsService;
+  let websocketService: jasmine.SpyObj<WebsocketService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EventsService);
+    const websocketSpy = jasmine.createSpyObj('WebsocketService', [
+      'onMessage',
+    ]);
+    TestBed.configureTestingModule({
+      providers: [
+        EventsService,
+        { provide: WebsocketService, useValue: websocketSpy },
+      ],
+    });
+    eventsService = TestBed.inject(EventsService);
+    websocketService = TestBed.inject(
+      WebsocketService
+    ) as jasmine.SpyObj<WebsocketService>;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(eventsService).toBeTruthy();
+  });
+
+  describe('findAllEventIds', () => {
+    it('should call onMessage with the correct topic', () => {
+      // Given
+      const expectedTopic = '/topic/inplay';
+      const mockBaseEvent = new Observable<BaseEvent[]>();
+      websocketService.onMessage.and.returnValue(mockBaseEvent);
+
+      // When
+      const result = eventsService.findAllEventIds();
+
+      // Then
+      expect(result).toBe(mockBaseEvent);
+      expect(websocketService.onMessage).toHaveBeenCalledWith(expectedTopic);
+    });
+  });
+
+  describe('findEventDetails', () => {
+    it('should call onMessage with the correct topic', () => {
+      // Given
+      const id = 123;
+      const expectedTopic = `/topic/event/${id}`;
+      const mockEvent = new Observable<Event>();
+      websocketService.onMessage.and.returnValue(mockEvent);
+
+      // When
+      const result = eventsService.findEventDetails(id);
+
+      // Then
+      expect(result).toBe(mockEvent);
+      expect(websocketService.onMessage).toHaveBeenCalledWith(expectedTopic);
+    });
+  });
+
+  describe('findMarketDetails', () => {
+    it('should call onMessage with the correct topic', () => {
+      // Given
+      const id = 456;
+      const expectedTopic = `/topic/market/${id}`;
+      const mockMarketEvent = new Observable<MarketEvent>();
+      websocketService.onMessage.and.returnValue(mockMarketEvent);
+
+      // When
+      const result = eventsService.findMarketDetails(id);
+
+      // Then
+      expect(result).toBe(mockMarketEvent);
+      expect(websocketService.onMessage).toHaveBeenCalledWith(expectedTopic);
+    });
   });
 });
